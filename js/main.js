7cefc48b55bd865581931fd1623d63bd2568a789
Irma-Martinez-Orozco <irma.159753@gmail.com>
2020-05-24 03:15:30 -0500

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(window).bind('scroll',function(){
+var navHeight=$(window).height()-70;
+if($(window).scrollTop()>navHeight){
+    $('nav').addClass('fixed');
+}
+else{
+    $('nav').removeClass('fixed');
+}
+    })
+})
+
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
